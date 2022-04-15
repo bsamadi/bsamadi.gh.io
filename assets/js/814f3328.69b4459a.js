668020c7a44e2cd1b4cab1d7393f1d15c8f70271
gh-actions <actions@gihub.com>
2022-04-15 22:34:23 +0000

    Deploy website - based on 2afc454d768a2aa87b463f938c00afd8240f75d7

---
+++ assets/js/814f3328.69b4459a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdatagit=self.webpackChunkdatagit||[]).push([[535],{6353:function(e){e.exports=JSON.parse('{"title":"\u062a\u0627\u0632\u0647\u200c\u0647\u0627","items":[{"title":"\u0646\u0635\u0628 Homebrew","permalink":"/coding/blog/homebrew-installation"}]}')}}]);
